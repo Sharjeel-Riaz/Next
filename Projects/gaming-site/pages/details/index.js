946bca74718645ca71ff882e270bdb9f5e05a1cf
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2023-11-05 06:23:01 +0500

    Started working on Contact section - details page

--- Projects/gaming-site/pages/details/index.js
+++ Projects/gaming-site/pages/details/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import simpleDoubleFull from "@/mock/simple-double-full";
 
 // Component imports
 import Navbar from "../../components/layout/Navbar";
 
 export default function Page() {
+  const [Brackets, setBrackets] = useState(null);
+
+  useEffect(() => {
+    if (!Brackets) {
+      import("@g-loot/react-tournament-brackets/").then((brackets) => {
+        setBrackets(brackets);
+      });
+    }
+  }, [Brackets]); // Added Brackets to the dependency array
+
   return (
     <>
       {/* Loading components */}
@@ -140,18 +151,77 @@ export default function Page() {
       </section>
 
       {/* Brackets section */}
-      <div className="mt-5">
-        <h2 className="text-xl font-bold text-gray-800">Brackets</h2>
-        <p className="mt-2 text-gray-600">
-          This is a temporary brackets section.
-        </p>
-      </div>
+      <section className="py-2 m-0">
+        <div className="mt-5 px-20 py-10">
+          <h2 className="text-3xl font-bold text-black">Brackets</h2>
+          <div className="mt-3 flex justify-between items-center">
+            <div className="relative bg-slate-100 flex items-center w-auto h-8 rounded-md focus-within:shadow-lg overflow-hidden">
+              <div className="grid place-items-center h-auto w-12 text-black">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+              <input
+                className="peer h-full w-full outline-none text-xs text-black placeholder:text-black pr-2 bg-slate-100"
+                type="text"
+                id="search"
+                placeholder="Search participants.."
+              />
+            </div>
+            <button className="bg-slate-100 text-black rounded-lg px-6 py-2 text-sm">
+              Matches
+            </button>{" "}
+          </div>
+          <div className="mx-auto my-10 p-5 bg-[#111827] rounded-lg text-white">
+            {Brackets && (
+              <Brackets.DoubleEliminationBracket
+                matches={simpleDoubleFull}
+                matchComponent={Brackets.Match}
+                svgWrapper={({ children, ...props }) => (
+                  <Brackets.SVGViewer
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    style={{
+                      maxWidth: "100%",
+                      borderRadius: "15px",
+                      overflow: "hidden",
+                    }}
+                    {...props}
+                  >
+                    {children}
+                  </Brackets.SVGViewer>
+                )}
+              />
+            )}
+          </div>
+        </div>
+      </section>
 
       {/* Media section */}
-      <div className="mt-5">
-        <h2 className="text-xl font-bold text-gray-800">Media</h2>
-        <p className="mt-2 text-gray-600">This is a temporary media section.</p>
-      </div>
+      <section className="py-2 m-0">
+        <div className="mt-5 px-20 py-10 bg-[#111827]">
+          <h2 className="text-3xl font-bold text-slate-200">Media</h2>
+          <img
+            className="mx-auto my-5 rounded-lg shadow-lg"
+            src="https://fakeimg.pl/800x200?font=noto-serif"
+            alt="Placeholder"
+          />{" "}
+          <p className="mt-2 text-slate-200">
+            There is currently no media available.
+          </p>
+        </div>
+      </section>
 
       {/* Contact section */}
       <div className="mt-5">
